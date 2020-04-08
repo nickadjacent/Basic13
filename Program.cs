73f83ef3abd9ca01426f899f857c358bb762378e
Nick_Ram <60027609+nickadjacent@users.noreply.github.com>
2020-04-08 15:59:46 -0700

    Basic 13

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Basic_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumbers();
+            PrintOdds();
+            PrintSum();
+            LoopArray(new int[] { 0, 1, 2, 3, 4, 5 });
+            FindMax(new int[] { -7, -9, -1, -8, -2, -6 });
+        }
+
+        public static void PrintNumbers()
+        {
+            // Print all of the integers from 1 to 255.
+            for (int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void PrintOdds()
+        {
+            // Print all of the odd integers from 1 to 255.
+            for (int i = 1; i <= 255; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static void PrintSum()
+        {
+            // Print all of the numbers from 0 to 255, 
+            // but this time, also print the sum as you go. 
+            // For example, your output should be something like this:
+
+            // New number: 0 Sum: 0
+            // New number: 1 Sum: 1
+            // New Number: 2 Sum: 3
+            int sum = 0;
+            for (int i = 0; i <= 255; i++)
+            {
+                sum += i;
+                Console.WriteLine("New Number: " + i + " Sum: " + sum);
+            }
+        }
+
+        public static void LoopArray(int[] numbers)
+        {
+            // Write a function that would iterate through each item of the given integer array and 
+            // print each value to the console.
+            // numbers = new int[] { 0, 1, 2, 3, 4 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+
+        public static int FindMax(int[] numbers)
+        {
+            // Write a function that takes an integer array and prints and returns the maximum value in the array. copy
+            // Your program should also work with a given array that has all negative numbers (e.g. [-3, -5, -7]), 
+            // or even a mix of positive numbers, negative numbers and zero
+            int max = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (max < numbers[i])
+                {
+                    max = numbers[i];
+                }
+            }
+            Console.WriteLine(max);
+            return max;
+        }
+    }
+}
